3ce6fac9a87a78aea99da2b1463793bbf45af77a
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2026-01-26 15:47:14 -0300

    style: changed TaskDialog

--- src/components/AddTaskDialog.jsx
+++ src/components/AddTaskDialog.jsx
@@ -1,5 +1,8 @@
 import { createPortal } from "react-dom"
 
+import Button from "./Button"
+import Input from "./Input"
+
 const AddTaskDialog = ({ isOpen }) => {
   if (!isOpen) return null
 
@@ -7,7 +10,23 @@ const AddTaskDialog = ({ isOpen }) => {
     <div className="fixed bottom-0 left-0 right-0 top-0 flex h-screen w-screen items-center justify-center backdrop-blur-sm">
       <div className="rounded-xl bg-white p-5 text-center shadow">
         <h2 className="text-xl font-semibold text-[#35383E]">Nova Tarefa</h2>
-        <p className="mt-1 text-sm text-[#9A9C9F]">Insira as informações</p>
+        <p className="mb-4 mt-1 text-sm text-[#9A9C9F]">
+          Insira as informações abaixo
+        </p>
+
+        <div className="flex w-[336px] flex-col space-y-4">
+          <Input id="title" label="Título" placeholder="Insira o título" />
+          <Input id="time" label="Horário" placeholder="Horário" />
+          <Input
+            id="description"
+            label="Descrição"
+            placeholder="Descreva a tarefa"
+          />
+          <div className="flex gap-3">
+            <Button className="w-full" size={"large"} text={"Cancelar"} />
+            <Button className="w-full" size={"large"} text={"Salvar"} />
+          </div>
+        </div>
       </div>
     </div>,
 
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,12 @@
-const Button = ({ icon, text, variant = "primary", onClick }) => {
+const Button = ({
+  icon,
+  text,
+  variant = "primary",
+  size = "small",
+  onClick,
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     if (variant === "primary") {
       return "bg-[#00ADB5] text-white"
@@ -8,10 +16,22 @@ const Button = ({ icon, text, variant = "primary", onClick }) => {
       return "bg-transparent text-[#818181]"
     }
   }
+
+  const getSizeClasses = () => {
+    if (size === "small") {
+      return "px-3 py-1 text-xs"
+    }
+
+    if (size === "large") {
+      return "px-3 py-2 text-sm"
+    }
+  }
+
   return (
     <button
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-75 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md font-semibold transition hover:opacity-75 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
       onClick={onClick}
+      {...rest}
     >
       {text}
       {icon}
